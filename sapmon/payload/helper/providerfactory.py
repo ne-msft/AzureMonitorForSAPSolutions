f54fa9f0e3d7ab211c05c586677e80ccaa9bb76b
Tobias Niekamp <tniek@microsoft.com>
2020-03-08 04:05:06 +0000

    Re-factor provider creation via ProviderFactory

---
+++ sapmon/payload/helper/providerfactory.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+
+#from provider.base import ProviderInstance, ProviderCheck
+from provider.saphana import *
+
+availableProviders = {
+                        "saphana": (saphanaProviderInstance, saphanaProviderCheck)
+                     }
+
+class ProviderFactory(object):
+   @staticmethod
+   def makeProviderInstance(providerType: str,
+                            tracer: logging.Logger,
+                            instanceProperties: Dict[str, str],
+                            **kwargs) -> ProviderInstance:
+      if providerType in availableProviders:
+         providerClass = availableProviders[providerType][0]
+         return providerClass(tracer,
+                              instanceProperties,
+                              **kwargs)
+      raise ValueError("unknown provider type %s" % providerType)
+
+   @staticmethod
+   def makeProviderCheck(providerType: str,
+                         providerInstance: ProviderInstance,
+                         **kwargs) -> ProviderCheck:
+      if providerType in availableProviders:
+         checkClass = availableProviders[providerType][1]
+         return checkClass(providerInstance,
+                           **kwargs)
+      raise ValueError("unknown provider type %s" % providerType)
