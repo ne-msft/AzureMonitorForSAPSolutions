179edbcd1b88f29b8c07b811f307bfe285d6228b
Clas Hortien <clash@microsoft.com>
2020-06-17 21:50:23 +0000

    Remove external KV logic

--- sapmon/payload/provider/sqlserver.py
+++ sapmon/payload/provider/sqlserver.py
@@ -47,7 +47,6 @@ class MSSQLProviderInstance(ProviderInstance):
 
    # Parse provider properties and fetch DB password from KeyVault, if necessary
    def parseProperties(self):
-
       self.SQLHostname = self.providerProperties.get("SQLHostname", None)
       if not self.SQLHostname:
          self.tracer.error("[%s] SQLHostname cannot be empty" % self.fullName)
@@ -58,39 +57,8 @@ class MSSQLProviderInstance(ProviderInstance):
          return False
       self.SQLPassword = self.providerProperties.get("SQLPassword", None)
       if not self.SQLPassword:
-         sqlDbPasswordKeyVaultUrl = self.providerProperties.get("sqlDbPasswordKeyVaultUrl", None)
-         passwordKeyVaultMsiClientId = self.providerProperties.get("keyVaultCredentialsMsiClientID", None)
-         if not sqlDbPasswordKeyVaultUrl or not passwordKeyVaultMsiClientId:
-            self.tracer.error("[%s] if no password, sqlDbPasswordKeyVaultUrl and keyVaultCredentialsMsiClientID must be given" % self.fullName)
-            return False
-
-         # Determine URL of separate KeyVault
-         self.tracer.info("[%s] fetching sql credentials from separate KeyVault" % self.fullName)
-         try:
-            passwordSearch = re.match(REGEX_EXTERNAL_KEYVAULT_URL,sqlDbPasswordKeyVaultUrl,re.IGNORECASE)
-            kvName = passwordSearch.group(1)
-            passwordName = passwordSearch.group(2)
-            passwordVersion = passwordSearch.group(4)
-         except Exception as e:
-            self.tracer.error("[%s] invalid URL format (%s)" % (self.fullName, e))
-            return False
-
-         # Create temporary KeyVault object to fetch relevant secret
-         try:
-            kv = AzureKeyVault(self.tracer,
-                               kvName,
-                               passwordKeyVaultMsiClientId)
-         except Exception as e:
-            self.tracer.error("[%s] error accessing the separate KeyVault (%s)" % (self.fullName,e))
-            return False
-
-         # Access the actual secret from the external KeyVault
-         # todo: proper (provider-independent) handling of external KeyVaults
-         try:
-            self.SQLPassword = kv.getSecret(SQLPassword, None).value
-         except Exception as e:
-            self.tracer.error("[%s] error accessing the secret inside the separate KeyVault (%s)" % (self.fullName,e))
-            return False
+         self.tracer.error("[%s] SQLPassword cannot be empty" % self.fullName)
+         return False
       return True
 
    # Validate that we can establish a sql connection and run queries
